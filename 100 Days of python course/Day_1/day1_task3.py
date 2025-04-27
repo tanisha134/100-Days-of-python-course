d9fa9466b761ecdac8c5ef536196c537fefc8d29
Jannatul Nur Tanisha <jannatulnurtanisha63@gmail.com>
2025-04-27 21:13:42 +0600

    Add files via upload

---
+++ 100 Days of python course/Day_1/day1_task3.py
@@ -0,0 +1,10 @@
+print("Hello world!\nHello world!\nHello world!")
+print("Hello"+" "+ "Tanisha")
+# Debugging practice
+# Fix the code below 👇
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
